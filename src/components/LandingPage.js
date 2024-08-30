a83993e09beee1bec6dc9c23513d9fe014a6c4bd
Ahmed Alghotmy <ahmedalghotmy@ahmeds-mbp-2.home>
2024-08-30 19:48:45 -0400

    updating the cognito links

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,7 +5,7 @@ import Header from './Header';
 import Footer from './Footer';
 import DemoRequestForm from './DemoRequestForm';
 import TestingAgentForm from './TestingAgentForm';
-import '../styles/LandingPage.css'; // Corrected import path
+import '../styles/LandingPage.css'; // Ensure the correct path to CSS
 
 const LandingPage = () => {
   return (
@@ -17,8 +17,18 @@ const LandingPage = () => {
           <h1>Welcome to ContactAI</h1>
           <p>Turn missed calls into revenue with AI phone call agents.</p>
           <div className="buttons">
-            <button onClick={() => window.location.href = '/signin'}>Sign In</button>
-            <button onClick={() => window.location.href = '/signup'}>Sign Up</button>
+            <button 
+              className="auth-button"
+              onClick={() => window.location.href = 'https://contactai.auth.ca-central-1.amazoncognito.com/login?client_id=6p6r9ps49qu1ehoom8apsm9jcm&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https%3A%2F%2Fmain.d2tf8n90uf18rf.amplifyapp.com%2Fdashboard%2F'}
+            >
+              Sign In
+            </button>
+            <button 
+              className="auth-button"
+              onClick={() => window.location.href = 'https://contactai.auth.ca-central-1.amazoncognito.com/signup?client_id=6p6r9ps49qu1ehoom8apsm9jcm&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https%3A%2F%2Fmain.d2tf8n90uf18rf.amplifyapp.com%2Fdashboard%2F'}
+            >
+              Sign Up
+            </button>
           </div>
         </section>
 
--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/SignOut.css'; // Corrected import path
+import '../styles/SignOut.css'; // Correct path
 
 const SignOut = () => {
   const navigate = useNavigate();
