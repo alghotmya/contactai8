cadb49f1e7f46b58df7d91038c533d6f919371f2
Ahmed Alghotmy <ahmedalghotmy@ahmeds-mbp-2.home>
2024-09-02 12:43:44 -0400

    Monday Sep 2- 4th  push -  updating some of the landing page and dashboard components - cleaning up dashboard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Dashboard from './components/Dashboard';
 import AuthHandler from './components/AuthHandler';
+import SignOut from './components/SignOut'; // Import the SignOut component
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<AuthHandler />} />  {/* Use AuthHandler to process the token */}
         <Route path="/main" element={<Dashboard />} /> {/* Redirect to the actual dashboard */}
+        <Route path="/signout" element={<SignOut />} /> {/* Route to handle signout */}
       </Routes>
     </Router>
   );
