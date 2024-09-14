a0299458cab4babee5073708376c8e1c793f2ddf
Ahmed Alghotmy <ahmedalghotmy@ahmeds-mbp-2.home>
2024-09-14 17:06:09 -0400

    Sept 14 1st push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+// File: src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Dashboard from './components/Dashboard';
+import Settings from './components/Settings';  // Import the new Settings component
 import AuthHandler from './components/AuthHandler';
-import SignOut from './components/SignOut'; // Import the SignOut component
+import SignOut from './components/SignOut';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<AuthHandler />} />  {/* Use AuthHandler to process the token */}
-        <Route path="/main" element={<Dashboard />} /> {/* Redirect to the actual dashboard */}
-        <Route path="/signout" element={<SignOut />} /> {/* Route to handle signout */}
+        <Route path="/dashboard" element={<AuthHandler />} />
+        <Route path="/main" element={<Dashboard />} />
+        <Route path="/settings" element={<Settings />} />  {/* Add the Settings route */}
+        <Route path="/signout" element={<SignOut />} />
       </Routes>
     </Router>
   );
